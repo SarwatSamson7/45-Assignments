10cec83dfb5a4f225a463d3ad43f32d1cbc42908
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 03:52:44 -0800

    Add files via upload

---
+++ think.ts
@@ -0,0 +1,5 @@
+let countries: string[] = ["China", "United States", "France", "United Kingdom", "Russia"];
+console.log("List of permanent members of Security Council:");
+for(let top of countries){
+    console.log(top)
+}
