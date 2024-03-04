4f814659a19e1e9140e78150c14d953b4ec1fdea
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 03:48:47 -0800

    Add files via upload

---
+++ array.ts
@@ -0,0 +1,5 @@
+let  favoriteCar:string[] = ["ROLLS-ROYCE", "BUGATTI", "LAMBORGHINI","FERRARI"];
+
+for (let i=0; i<favoriteCar.length; i++) {
+    console.log(`I would like to own a` + favoriteCar[i]);
+}
