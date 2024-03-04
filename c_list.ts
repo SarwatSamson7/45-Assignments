e50755d6640b8ba1af5b73a4ce3c3164972181c2
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 03:50:16 -0800

    Add files via upload

--- c_list.ts
+++ c_list.ts
@@ -1,8 +1,8 @@
 let guest_list:string[] = ["Shireen", "Rizwana", "Aneeqa"];
 
-for(let i=0; i<guest_list.length; i++) {
-    console.log(`Dear ${guest_list},\n\nlet's celebrate the joy of togetherness with good food and great friends. Join us for a dinner party at our home.\n\nThank you`);
-}
+// for(let i=0; i<guest_list.length; i++) {
+//     console.log(`Dear ${guest_list},\n\nlet's celebrate the joy of togetherness with good food and great friends. Join us for a dinner party at our home.\n\nThank you`);
+// }
 
 let absent_guest: string = "Rizwana";
 
@@ -14,4 +14,17 @@ for(let i=0; i<guest_list.length; i++) {
     console.log(`Dear ${guest_list},\n\nlet's celebrate the joy of togetherness with good food and great friends. Join us for a dinner party at our home.\n\nThank you`);
 }
 
-console.log(`Mrs. ${absent_guest} is not coming.`)
+console.log(`Mrs. ${absent_guest} is not coming.`);
+
+console.log(`Good News! We find a big dinner table so, we are inviting 3 more guests.`)
+
+guest_list.unshift('Anna');
+guest_list.splice(2,0,'Abigail');
+guest_list.push('Araina');
+
+for(let i=0; i<guest_list.length; i++) {
+    console.log(`Dear ${guest_list},\n\nlet's celebrate the joy of togetherness with good food and great friends. Join us for a dinner party at our home.\n\nThank you`);
+}
+
+
+
