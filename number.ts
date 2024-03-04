8d91b20e213324fa952cce2f6debd32fab718843
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 04:00:33 -0800

    Add files via upload

--- number.ts
+++ number.ts
@@ -1,4 +1,20 @@
-console.log(4 + 4);
-console.log(12 - 4);
-console.log(4 * 2);
-console.log(24 / 3);
+let numbers: number[] = [1,2,3,4,5,6,7,8,9];
+
+//Loop through the array
+for (let number of numbers) {
+    let ordinalEnding: string;
+
+    if (number ===1) {
+        ordinalEnding = "st";
+    } else if (number === 2) {
+        ordinalEnding = 'nd';
+    } else if (number === 3) {
+        ordinalEnding = 'rd';
+    } else {
+        ordinalEnding = 'th';
+    }
+
+    
+console.log(`${number}${ordinalEnding}`);
+
+}
