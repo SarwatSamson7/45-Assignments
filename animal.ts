4607ade96db892035bf5ed43000c8a1cc8654173
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:14:37 -0800

    Delete animal.ts

--- animal.ts
+++
@@ -1,7 +0,0 @@
-let commonCharacteristic: string = 'They are mammals';
-let animals: string[] = ['Lion', 'Dog', 'Goat'];
-
-for (let animal of animals) {
-    console.log(`A ${animal.toLowerCase()} would make a great pet.`);
-}
-console.log(commonCharacteristic);
