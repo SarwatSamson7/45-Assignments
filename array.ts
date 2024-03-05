d8684e27a174ad2b80b9a24ac3ccd1463a154270
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:25:27 -0800

    Delete array.ts

--- array.ts
+++
@@ -1,5 +0,0 @@
-let  favoriteCar:string[] = ["ROLLS-ROYCE", "BUGATTI", "LAMBORGHINI","FERRARI"];
-
-for (let i=0; i<favoriteCar.length; i++) {
-    console.log(`I would like to own a` + favoriteCar[i]);
-}
