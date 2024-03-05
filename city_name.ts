ec28b5509b4926b8879e9ee6b47bdb27af31d803
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:26:04 -0800

    Delete city_name.ts

--- city_name.ts
+++
@@ -1,6 +0,0 @@
-let country_city = (city:string, country: string) =>{
-    return `"${city},${country}"`;
-}
-console.log(country_city("Quetta","Pakistan"));
-console.log(country_city("Lahore","Pakistan"));
-console.log(country_city("Karachi","Pakistan"));
