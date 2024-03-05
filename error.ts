7a8bcf1703f67ec282588b7c169f2b6d125bc36d
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:27:19 -0800

    Delete error.ts

--- error.ts
+++
@@ -1,7 +0,0 @@
-let array: string[] = ["Shireen", "Rizwana", "Aneeqa", "Maria", "Saman"];
-
-console.log(array[3]);
-console.log(array[4]);
-console.log(array[2]);
-console.log(array[0]);
-console.log(array[1]);
