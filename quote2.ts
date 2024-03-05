a86da0b0103f90b115db62da02bf4b131d856fd2
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:29:21 -0800

    Delete quote2.ts

--- quote2.ts
+++
@@ -1,4 +0,0 @@
-let quote: string= "A person who never made a mistake never tried anything new";
-let famouse_person: string= "Albert Einstein";
-let message = `${famouse_person} once said, ${quote}`;
-console.log(message);
