8693813af4da92734f1771c6b73c9afe2bed552c
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:30:02 -0800

    Delete pizza.ts

--- pizza.ts
+++
@@ -1,7 +0,0 @@
-let fav_Pizza: string[] = ["Vegetable Pizza", "Fajita Pizza", "Tikka Pizza"];
-
-for (let pizza of fav_Pizza) {
-    console.log(`I Like ${pizza}`);
-}
-
-console.log("I really love pizza!");
