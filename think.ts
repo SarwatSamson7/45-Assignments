2e940545bc1ab7bd54bb7cb145c55d459079dbef
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:17:14 -0800

    Delete think.ts

--- think.ts
+++
@@ -1,5 +0,0 @@
-let countries: string[] = ["China", "United States", "France", "United Kingdom", "Russia"];
-console.log("List of permanent members of Security Council:");
-for(let top of countries){
-    console.log(top)
-}
