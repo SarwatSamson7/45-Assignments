7ad0329360fa8ea76b4b42bee85f2f11b8b649ca
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:31:02 -0800

    Delete list.ts

--- list.ts
+++
@@ -1,5 +0,0 @@
-let guest_list:string[] = ["Shireen", "Rizwana", "Aneeqa"];
-
-for(let i=0; i<guest_list.length; i++) {
-    console.log(`Dear ${guest_list},\n\nlet's celebrate the joy of togetherness with good food and great friends. Join us for a dinner party at our home.\n\nThank you`);
-}
