1dfa056dce660796cc5907398badcc500a472e45
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2024-03-04 21:30:38 -0800

    Delete large.ts

--- large.ts
+++
@@ -1,8 +0,0 @@
-function make_shirt(size = "large", message = "I realy love typescript") {
-    
-    console.log("This is " + size + " shirt")
-    console.log(message)
-}
-
-make_shirt()
-make_shirt("medium")
